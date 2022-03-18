b9bd476a4223eada200320b825f4f2695a03acb7
MarcusLeao <30932349+MarcusLeao@users.noreply.github.com>
2022-03-18 12:35:42 -0300

    Add files via upload

---
+++ MySQL/teste.sql
@@ -0,0 +1,17 @@
+-- CREATE TABLE usuarios(
+--     nome VARCHAR(50),
+--     email VARCHAR(100),
+--     idade INT
+-- );
+
+-- INSERT INTO usuarios(nome, email, idade) VALUES(
+--     "Nemo",
+--     "neminhogostoso@gmail.com",
+--     8
+-- );
+
+-- SELECT * FROM usuarios WHERE idade <= 50;
+
+-- DELETE FROM usuarios WHERE nome = "Edwiges Selva";
+
+UPDATE usuarios SET email = "dudamorais1123@gmail.com" WHERE nome = "Eduarda Morais";
